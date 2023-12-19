53edd63a8093d83cff1d08058ab126644f523d91
t3mye <babatundetemi1@yahoo.com>
2023-12-19 11:40:46 +0100

    Intergrate firebase and google-sign-in

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:reddit/features/auth/screens/signin_screen.dart';
 import 'package:reddit/theme/palette.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
