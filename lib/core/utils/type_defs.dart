09d203f1623c62bfb2063a2345b1befe4c41993a
t3mye <babatundetemi1@yahoo.com>
2024-01-29 09:58:10 +0100

    Update with routing and response handling packages

--- lib/core/utils/type_defs.dart
+++ lib/core/utils/type_defs.dart
@@ -1,5 +1,7 @@
 import 'package:fpdart/fpdart.dart';
 import 'package:reddit/core/utils/failure.dart';
-
+/// FutureEither to handle failure and success cases
 typedef FutureEither<T> = Future<Either<Failure, T>>;
+
+/// FutureVoid to handle failure and void cases
 typedef FutureVoid = FutureEither<void>;
